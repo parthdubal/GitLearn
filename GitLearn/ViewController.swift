e71c1ff4cc0a4a625eb563810f3acf927e8df992
Parth Dubal <parth@dubal.com>
2017-02-22 16:23:32 +0530

    solved variable issue

--- GitLearn/ViewController.swift
+++ GitLearn/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let n = 5
         for i in 1...n
         {
             for _ in 0..<i
